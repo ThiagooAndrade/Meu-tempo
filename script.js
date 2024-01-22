572c3f774c657ebce17f600d9d7bea241f74cc1e
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-01-21 22:41:17 -0300

    a

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const buttonSearchCity = document.getElementById("btnSubmit");
-const apiKey = "62d536c6f9e3d37c3b29599bdf58880b";
-const url = `https://api.openweathermap.org/data/2.5/units=metric&lang=pt_br&appid=${apiKey}`;
+const apiKey = "";
+const url = ``;
 
 buttonSearchCity.addEventListener("click", () => {});
