6ba5928eef09c3a0448499b0d5fdd94d369e8390
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-01-23 00:40:18 -0300

    design quase finalizado

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 const buttonSearchCity = document.getElementById("btnSubmit");
+const container = document.querySelector(".container");
+const notFound = document.querySelector(".not-found");
+const weatherContainer = document.querySelector(".weather-container");
+const img = document.querySelector(".img");
+const temperature = document.querySelector(".temperature");
+const description = document.querySelector(".description");
+const humidityText = document.querySelector(".humidity-text");
+const windtext = document.querySelector(".wind-text");
+
 const apiKey = "";
 const url = ``;
 
-buttonSearchCity.addEventListener("click", () => {});
+buttonSearchCity.addEventListener("click", () => {
+  container.style.height = "70%";
+  weatherContainer.style.display = "flex";
+
+  temperature.textContent = "50Celcius";
+  description.textContent = "solzao";
+  humidityText.textContent = "50mm";
+  humidityText.style.visibility = "visible";
+  windtext.textContent = "10m/s";
+  windtext.style.visibility = "visible";
+});
