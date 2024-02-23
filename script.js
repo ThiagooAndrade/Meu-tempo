0f2936f43f4d41149c45e5db8e576b3fa5382982
ThiagooAndrade <andradethiago1609@Gmail.com>
2024-02-23 00:55:42 -0300

    changed async await to then

--- script.js
+++ script.js
@@ -12,21 +12,25 @@ buttonLocation.addEventListener("click", getLocation);
 
 function getLocation() {
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(async (position) => {
+        navigator.geolocation.getCurrentPosition((position) => {
             try {
                 const lat = position.coords.latitude;
                 const long = position.coords.longitude;
 
                 const nominatimURL = `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${long}&format=jsonv2&addressdetails=0&zoom=10`;
-                const response = await fetch(nominatimURL);
-                const data = await response.json();
-                console.log(data);
-                const resultCity = data.name;
+                fetch(nominatimURL)
+                    .then((response) => response.json())
+                    .then((data) => {
+                        console.log(data);
+                        const resultCity = data.name;
 
-                const city = document.querySelector(".container-input input");
-                city.value = resultCity;
-                searchCity(resultCity); // passa como parametro a cidade do usuario
-                /* geolocation is available */
+                        const city = document.querySelector(
+                            ".container-input input"
+                        );
+                        city.value = resultCity;
+                        searchCity(resultCity); // passa como parametro a cidade do usuario
+                        /* geolocation is available */
+                    });
             } catch {
                 window.alert("Ocorreu um erro");
             }
@@ -38,7 +42,7 @@ function getLocation() {
     }
 }
 
-async function searchCity(city) {
+function searchCity(city) {
     const weatherAPIKey = "3d2d4073bc04d77eb884358684f252cc";
     const weatherAPIURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=pt_br&appid=${weatherAPIKey}`;
 
@@ -51,70 +55,72 @@ async function searchCity(city) {
         notFound.style.display = "none";
     }
 
-    const response = await fetch(weatherAPIURL);
+    fetch(weatherAPIURL)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.cod === "404") {
+                const error404 = document.querySelector(".error404");
+                container.classList.add("container-height");
+                weatherContainer.style.display = "none";
+                weatherDetails.style.display = "none";
+                notFound.style.display = "block";
+                error404.classList.add("fadeIn");
+                return;
+            }
 
-    const data = await response.json();
+            container.classList.add("container-height");
 
-    if (data.cod === "404") {
-        const error404 = document.querySelector(".error404");
-        container.classList.add("container-height");
-        weatherContainer.style.display = "none";
-        weatherDetails.style.display = "none";
-        notFound.style.display = "block";
-        error404.classList.add("fadeIn");
-        return;
-    }
+            const img = document.querySelector(".img");
+            switch (data.weather[0].main) {
+                case "Clear":
+                    img.src = "assets/clear.png";
+                    break;
 
-    container.classList.add("container-height");
+                case "Clouds":
+                    img.src = "assets/cloud.png";
+                    break;
 
-    const img = document.querySelector(".img");
-    switch (data.weather[0].main) {
-        case "Clear":
-            img.src = "assets/clear.png";
-            break;
+                case "Mist":
+                    img.src = "assets/mist.png";
+                    break;
 
-        case "Clouds":
-            img.src = "assets/cloud.png";
-            break;
+                case "Haze":
+                    img.src = "assets/haze.png";
+                    break;
 
-        case "Mist":
-            img.src = "assets/mist.png";
-            break;
+                case "Rain":
+                    img.src = "assets/rain.png";
+                    break;
 
-        case "Haze":
-            img.src = "assets/haze.png";
-            break;
+                case "Snow":
+                    img.src = "assets/snow.png";
+                    break;
 
-        case "Rain":
-            img.src = "assets/rain.png";
-            break;
+                default:
+                    img.src = "";
+            }
 
-        case "Snow":
-            img.src = "assets/snow.png";
-            break;
+            weatherContainer.classList.add("fadeIn");
+            weatherDetails.classList.add("fadeIn");
 
-        default:
-            img.src = "";
-    }
+            weatherContainer.style.display = "flex";
+            weatherDetails.style.display = "flex";
 
-    weatherContainer.classList.add("fadeIn");
-    weatherDetails.classList.add("fadeIn");
+            const temperature = document.querySelector(".temperature");
+            temperature.innerHTML = `${parseInt(
+                data.main.temp
+            )}<span>°C</span>`;
 
-    weatherContainer.style.display = "flex";
-    weatherDetails.style.display = "flex";
+            const description = document.querySelector(".description");
+            description.innerHTML = `${data.weather[0].description}`;
+            description.style.textTransform = "capitalize";
 
-    const temperature = document.querySelector(".temperature");
-    temperature.innerHTML = `${parseInt(data.main.temp)}<span>°C</span>`;
+            const humidity = document.querySelector(".humidity-text");
+            humidity.innerHTML = `${data.main.humidity}%`;
+            humidity.style.visibility = "visible";
 
-    const description = document.querySelector(".description");
-    description.innerHTML = `${data.weather[0].description}`;
-    description.style.textTransform = "capitalize";
-
-    const humidity = document.querySelector(".humidity-text");
-    humidity.innerHTML = `${data.main.humidity}%`;
-    humidity.style.visibility = "visible";
-
-    const wind = document.querySelector(".wind-text");
-    wind.innerHTML = `${data.wind.speed} KM/h`;
-    wind.style.visibility = "visible";
+            const wind = document.querySelector(".wind-text");
+            wind.innerHTML = `${data.wind.speed} KM/h`;
+            wind.style.visibility = "visible";
+        });
 }
